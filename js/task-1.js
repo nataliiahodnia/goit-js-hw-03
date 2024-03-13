52430aba6fe3bdf0b5dc03f166c820e7fbe95b78
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-03-13 15:27:56 +0100

    added 3HW

--- js/task-1.js
+++ js/task-1.js
@@ -1,33 +1,8 @@
-// * version 1* //
-// function makeTransaction(quantity, pricePerDroid, customerCredits) {
-//   const totalPrice = quantity * pricePerDroid;
-//   let message = "";
-//   if (totalPrice > customerCredits) {
-//     message = "Insufficient funds!";
-//   } else {
-//     message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-//   }
-//   alert(message);
-//   return message;
-// }
-
-// console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-// console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-// console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-// console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-// console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
-
-// * version 2* //
-
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-  return totalPrice > customerCredits
-    ? "Insufficient funds!"
-    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+function slugify(title) {
+  return title.toLowerCase().split(' ').join('-');
 }
 
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
